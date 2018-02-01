2dfc66893e670759966b2fda6bff922260e672b5
Unknown <yuhui_tan@mymail.sutd.edu.sg>
2018-02-01 18:54:32 +0800

    Init commit
    Init commit

---
+++ app/src/main/java/com/example/fish/escpart1/StartUpActivity.java
@@ -0,0 +1,14 @@
+package com.example.fish.escpart1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+
+public class StartUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_up);
+    }
+}
