470e4890e6e26b508620be61773f61fedfaad63e
Vincentcent1 <Vincent@mymail.sutd.edu.sg>
2018-03-15 02:11:34 +0800

    Combined with camera and server. Login not implemented with camera yet.

--- ESCIBM/app/src/main/java/com/example/fish/escpart1/CameraActivity.java
+++ ESCIBM/app/src/main/java/com/example/fish/escpart1/CameraActivity.java
@@ -1,76 +1,96 @@
-package com.example.user.trial_android_sql;
+package com.example.fish.escpart1;
 
 import android.Manifest;
-import android.app.DownloadManager;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
-import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.Base64;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
 import java.io.ByteArrayOutputStream;
-import java.io.IOException;
+import java.util.HashMap;
 
 
 public class CameraActivity extends AppCompatActivity {
-    private ImageView Display_taken_image;
-    private int camerarequest=432; //integer to tell you what activity was run. Use different codes for different parts! i.e. later when you upload your images.
-    private int request_camera_permissions = 5050; //integer to tell me i did a permissions activity for the camera
+    private ImageView imageTaken;
+    private int CAMERA_REQUEST =432; //integer to tell you what activity was run. Use different codes for different parts! i.e. later when you upload your images.
+    private int CAMERA_PERMISSION_REQUEST = 5050; //integer to tell me i did a permissions activity for the camera
+    private HashMap<String,String> params;
+    private Button registerButton, takePictureButton;
+    private String encodedCamera;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
-        Requestpermissions();
-        Display_taken_image = (ImageView) findViewById(R.id.camerapictureview); //the image to show picture taken.
-        findViewById(R.id.take_a_pic).setOnClickListener(new View.OnClickListener(){
+        imageTaken = (ImageView) findViewById(R.id.imagePlaceholderCamera); //the image to show picture taken.
+        params = (HashMap<String, String>) getIntent().getSerializableExtra("params");
+
+        Requestpermissions(); //TODO:Need to always request permission?
+
+        takePictureButton = findViewById(R.id.takePictureButtonCamera);
+        takePictureButton.setOnClickListener(new View.OnClickListener(){
             public void onClick(View view){
                 opencamera();
             }
         });
+
+        registerButton = findViewById(R.id.registerButtonCamera);
+        registerButton.setOnClickListener(new View.OnClickListener(){
+
+            @SuppressWarnings("unchecked")
+            public void onClick(View view) {
+                params.put("face", encodedCamera);
+                RegisterUser ru = new RegisterUser(getApplicationContext());
+                for (String i: params.keySet()) {
+                    System.out.println(i + params.get(i));
+                }
+                ru.execute(params);
+            }
+        });
+
+
     }
     private void opencamera(){
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        startActivityForResult(intent, this.request_camera_permissions); // i placed a random number here because we don't care what results from it..
+        startActivityForResult(intent, this.CAMERA_REQUEST); // i placed a random number here because we don't care what results from it..
     }
 
     private void Requestpermissions() {
         ActivityCompat.requestPermissions(this,
                 new String[]{Manifest.permission.CAMERA},
-                this.request_camera_permissions); //camerarequest is a request code. just to distinguish so that onActivityResult can distinguish
+                this.CAMERA_PERMISSION_REQUEST); //CAMERA_REQUEST is a request code. just to distinguish so that onActivityResult can distinguish
     }
     @Override
     //So this onActivityResult is so that you can get the image thumbnail and prepare it for sending.
-    //I ended the code here on encoded_camera because i don't know what else you might need to do.
-    //but anyway just replace the string face_encoded with encoded_camera when you want to send the POST request over.
+    //I ended the code here on encoded_Cmera because i don't know what else you might need to do.
+    //but anyway just replace the string face_encoded with encoded_Cmera when you want to send the POST request over.
     //face_encoded is for registration (although you can potentially use more face_encoded s when you need to send multiple face images.
     //You also need to tell me what you called each of the keys in params when you're sending the json file over.
     //or my server will reject it.
     public void onActivityResult(int requestCode, int resultCode, Intent data){
         //STACK WITH YOUR OTHER IMAGE SELECTS, ETC.
-        if(requestCode == this.camerarequest){ // i.e the request code was that of the camera...
+        if(requestCode == this.CAMERA_REQUEST){ // i.e the request code was that of the camera...
             Bitmap item = (Bitmap) data.getExtras().get("data"); //so i make it a bitmap
             ByteArrayOutputStream bytes = new ByteArrayOutputStream(); //need this.
             item.compress(Bitmap.CompressFormat.JPEG, 100, bytes); //into bytes.
             byte[] imagebytes_camera = bytes.toByteArray();
-            String encoded_camera = Base64.encodeToString(imagebytes_camera, Base64.DEFAULT);
-            this.Display_taken_image.setImageBitmap(item);
+            encodedCamera = Base64.encodeToString(imagebytes_camera, Base64.DEFAULT);
+            this.imageTaken.setImageBitmap(item);
+            System.out.println("asd");
+            registerButton.setVisibility(View.VISIBLE);
         }
     }
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        if (requestCode == request_camera_permissions) { //indicate that it was camera request permissions. STACK WITH YOUR OTHER PERMISSIONS
+        if (requestCode == CAMERA_PERMISSION_REQUEST) { //indicate that it was camera request permissions. STACK WITH YOUR OTHER PERMISSIONS
 
             if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 //If permission is granted
@@ -87,4 +107,4 @@ public class CameraActivity extends AppCompatActivity {
     }
 
 }
-    //PS this WILL crash the entire app if camera permissions weren't granted.
+    //TODO:PS this WILL crash the entire app if camera permissions weren't granted.
--- ESCIBM/app/src/main/java/com/example/fish/escpart1/RegisterActivity.java
+++ ESCIBM/app/src/main/java/com/example/fish/escpart1/RegisterActivity.java
@@ -1,24 +1,14 @@
 package com.example.fish.escpart1;
 
-import android.os.AsyncTask;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ProgressBar;
 import android.widget.Spinner;
 import android.widget.Toast;
-
-import com.example.fish.sqlJava.RequestHandler;
-import com.example.fish.sqlJava.SharedPrefManager;
-import com.example.fish.sqlJava.URLs;
-import com.example.fish.sqlJava.User;
-
-import org.json.JSONException;
-import org.json.JSONObject;
 
 import java.util.HashMap;
 
@@ -28,7 +18,7 @@ public class RegisterActivity extends AppCompatActivity {
     String[] genders_array = {"M", "F", "Others"};
     Spinner designationSpinner, genderSpinner;
     EditText firstNameEditText, lastNameEditText, nricEditText, dobEditText,
-            homeAddressEditText, postalCodeEditText, contactNumberEditText, emailEditText, passwordEditText, verifyPasswordEditText;
+            homeAddressEditText, postalCodeEditText, contactNumberEditText, emailEditText, passwordEditText, verifyPasswordEditText, nationalityEditText;
     String password, verifyPassword;
 
     @Override
@@ -57,6 +47,7 @@ public class RegisterActivity extends AppCompatActivity {
         postalCodeEditText = findViewById(R.id.postalCodeEditView);
         contactNumberEditText = findViewById(R.id.contactNumberEditView);
         emailEditText = findViewById(R.id.emailEditView);
+        nationalityEditText = findViewById(R.id.nationalityEditView);
 
         genderSpinner.setAdapter(genderSpinnerAdapter);
         passwordEditText = findViewById(R.id.passwordEditView);
@@ -66,30 +57,31 @@ public class RegisterActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 HashMap<String,String> params = new HashMap<>();
-                password = passwordEditText.getText().toString();
+                password = passwordEditText.getText().toString().trim();
                 verifyPassword = verifyPasswordEditText.getText().toString();
                 if (!verifyPassword(password, verifyPassword)){
                     return;
                 }
-                params.put("username", emailEditText.getText().toString());
-                params.put("firstname", firstNameEditText.getText().toString());
-                params.put("lastname", lastNameEditText.getText().toString());
-                params.put("NRIC", nricEditText.getText().toString());
-                params.put("DOB", dobEditText.getText().toString());
-                params.put("Address", homeAddressEditText.getText().toString());
-                params.put("postalcode", postalCodeEditText.getText().toString());
-                params.put("contact", contactNumberEditText.getText().toString());
-                params.put("email", emailEditText.getText().toString());
-                params.put("accountnumber", "1234"); //TODO: make this autoincrement in server
-//                params.put("", designationSpinner.getSelectedItem().toString());
-                params.put("amount", "0"); //TODO: make this initialised to zero in server
+                params.put("username", emailEditText.getText().toString().trim());
+                params.put("firstname", firstNameEditText.getText().toString().trim());
+                params.put("lastname", lastNameEditText.getText().toString().trim());
+                params.put("NRIC", nricEditText.getText().toString().trim());
+                params.put("DOB", dobEditText.getText().toString().trim());
+                params.put("Address", homeAddressEditText.getText().toString().trim());
+                params.put("postalcode", postalCodeEditText.getText().toString().trim());
+                params.put("contact", contactNumberEditText.getText().toString().trim());
+                params.put("email", emailEditText.getText().toString().trim());
                 params.put("gender",genderSpinner.getSelectedItem().toString());
                 params.put("password", password);
+                params.put("Designation", designationSpinner.getSelectedItem().toString());
+                params.put("Nationality", nationalityEditText.getText().toString().trim());
+                Intent intent = new Intent(getApplicationContext(), CameraActivity.class);
+                intent.putExtra("params", params);
+                startActivity(intent);
 
-
-                RegisterUser ru = new RegisterUser();
-                ru.execute(params);
                 Toast.makeText(RegisterActivity.this, "Details saved", Toast.LENGTH_SHORT).show();
+
+
             }
         });
 
@@ -104,67 +96,4 @@ public class RegisterActivity extends AppCompatActivity {
         }
         return true;
     }
-    class RegisterUser extends AsyncTask<HashMap<String,String>, Void, String> {
-
-        private ProgressBar progressBar;
-
-        @Override
-        protected String doInBackground(HashMap<String,String>... inputParams) {
-            //creating request handler object
-            RequestHandler requestHandler = new RequestHandler();
-
-            //returning the response
-            return requestHandler.sendPostRequest(URLs.URL_REGISTER, inputParams[0]);
-        }
-
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-            //displaying the progress bar while user registers on the server
-//            progressBar = (ProgressBar) findViewById(R.id.progressBar);
-//            progressBar.setVisibility(View.VISIBLE);
-        }
-
-        @Override
-        protected void onPostExecute(String s) {
-            super.onPostExecute(s);
-            //hiding the progressbar after completion
-//            progressBar.setVisibility(View.GONE);
-
-            try {
-                //converting response to json object
-                JSONObject obj = new JSONObject(s);
-                Log.i("Register",s);
-                //if no error in response
-                if (!obj.getBoolean("error")) {
-                    Toast.makeText(getApplicationContext(), obj.getString("message"), Toast.LENGTH_SHORT).show();
-
-                    //getting the user from the response
-                    JSONObject userJson = obj.getJSONObject("user");
-
-                    //creating a new user object
-                    User user = new User(
-                            userJson.getString("username"),
-                            userJson.getString("email"),
-                            userJson.getString("gender")
-                    );
-
-                    //storing the user in shared preferences
-                    SharedPrefManager.getInstance(getApplicationContext()).userLogin(user);
-
-                    //starting the profile activity
-                    finish();
-//                    TODO: Make a Profile Activity and start it after registration
-//                    startActivity(new Intent(getApplicationContext(), ProfileActivity.class));
-                } else {
-                    Toast.makeText(getApplicationContext(), "Some error occurred", Toast.LENGTH_SHORT).show();
-                }
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
-    //executing the async task
-
 }
