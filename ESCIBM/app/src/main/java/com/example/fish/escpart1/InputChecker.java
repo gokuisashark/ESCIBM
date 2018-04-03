49b46fb1c7134e4e7852d30ccc7d4441b2ec2952
Vincentcent1 <Vincent@mymail.sutd.edu.sg>
2018-04-03 19:42:55 +0800

    simple fix inputchecker.java

--- ESCIBM/app/src/main/java/com/example/fish/escpart1/InputChecker.java
+++ ESCIBM/app/src/main/java/com/example/fish/escpart1/InputChecker.java
@@ -50,7 +50,7 @@ public class InputChecker {
 
 
 
-    private boolean emailChecker(String email) {
+    boolean emailChecker(String email) {
         String ePattern =
                 "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$";
         java.util.regex.Pattern p = java.util.regex.Pattern.compile(ePattern);
